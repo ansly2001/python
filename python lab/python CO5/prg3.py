b9c1aa0e0aa002936de180f34854aad40f37a317
ansly2001 <120626700+ansly2001@users.noreply.github.com>
2023-01-18 06:39:36 -0800

    3

---
+++ python lab/python CO5/prg3.py
@@ -0,0 +1,7 @@
+import pandas as pd
+df=pd.read_csv("student detail1.csv",usecols=["roll no","name"])
+print(df)
+specific_column=df[["name","class"]]
+print(specific_column)
+print(df.head())
+print(df.tail())
